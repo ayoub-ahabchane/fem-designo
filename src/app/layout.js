4f28b6cff103ba6255aeca21336a812a69d1c633
ayoub_ahabchane <mr.ayoub.ahabchane@gmail.com>
2023-03-12 18:29:04 +0100

    fix: set minimum html height to viewpoer height and adjust body background position

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ const jost = Jost({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${jost.className}`}>
-      <body className="bg-[size:1006px_594px] bg-[position:top_475px_left,_right_top_1880px] bg-no-repeat lg:bg-[url(../assets/shared/desktop/bg-pattern-leaf.svg),_url(../assets/exports/rectangle-right-aligned.svg)]">
+      <body className="bg-[size:1006px_594px] bg-[position:top_475px_left,_right_bottom_475px] bg-no-repeat lg:bg-[url(../assets/shared/desktop/bg-pattern-leaf.svg),_url(../assets/exports/rectangle-right-aligned.svg)]">
         <Header />
         <main className="mb-[120px] grow pt-[98px] md:mb-[80px] md:px-10 md:pt-[154px] lg:mb-[160px]">
           {children}
