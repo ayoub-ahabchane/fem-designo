fb5271f218e34b286cb03188135475d1888e7611
ayoub_ahabchane <mr.ayoub.ahabchane@gmail.com>
2023-03-18 17:45:26 +0100

    fix: change heading tags in the qualities section to h2

--- src/components/home/Qualities.jsx
+++ src/components/home/Qualities.jsx
@@ -9,7 +9,7 @@ const Qualities = () => {
       <article className="flex flex-col items-center gap-12 text-center md:flex-row md:text-left lg:flex-col lg:justify-center lg:text-center">
         <Passionate />
         <div>
-          <h3 className="h3 mb-8 uppercase md:mb-4 lg:mb-8">Passionate</h3>
+          <h2 className="h3 mb-8 uppercase md:mb-4 lg:mb-8">Passionate</h2>
           <p className="max-w-[28rem] text-body">
             Each project starts with an in-depth brand research to ensure we
             only create products that serve a purpose. We merge art, design, and
@@ -20,7 +20,7 @@ const Qualities = () => {
       <article className="flex flex-col items-center gap-12 text-center md:flex-row md:text-left lg:flex-col lg:justify-center lg:text-center">
         <Resourceful />
         <div>
-          <h3 className="h3 mb-8 uppercase md:mb-4 lg:mb-8">Resourceful</h3>
+          <h2 className="h3 mb-8 uppercase md:mb-4 lg:mb-8">Resourceful</h2>
           <p className="max-w-[28rem] text-body">
             Everything that we do has a strategic purpose. We use an agile
             approach in all of our projects and value customer collaboration. It
@@ -31,7 +31,7 @@ const Qualities = () => {
       <article className="flex flex-col items-center gap-12 text-center md:flex-row md:text-left lg:flex-col lg:justify-center lg:text-center">
         <Friendly />
         <div>
-          <h3 className="h3 mb-8 uppercase md:mb-4 lg:mb-8">Friendly</h3>
+          <h2 className="h3 mb-8 uppercase md:mb-4 lg:mb-8">Friendly</h2>
           <p className="max-w-[28rem] text-body">
             We are a group of enthusiastic folks who know how to put people
             first. Our success depends on our customers, and we strive to give
